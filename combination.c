0ee062d6873bd596e71efb962b1d24bf9132488c
dedetmix <dedetmix@yahoo.co.id>
2017-11-01 20:41:15 +0100

    second commit

--- combination.c
+++ combination.c
@@ -1,67 +1,15 @@
-// Program to print all combination of size r in an array of size n
-// source code: geeksforgeeks.org [Bateesh] modified by Noorlaila Isya (2017)
-
 #include <stdio.h>
-#include <stdlib.h>
+// N.Isya 2017.11.01
 
-void combinationUtil(int arr[], int data[], int start, int end, 
-                     int index, int r);
- 
-// The main function that prints all combinations of size r
-// in arr[] of size n. This function mainly uses combinationUtil()
-void printCombination(int arr[], int n, int r)
+int main (int argc, char **argv, int *arr)
 {
+  //remove dummy result
+  char filename[] = "result_combination.txt";
+  remove(filename);
 
-    // A temporary array to store all combination one by one
-    int data[r];
- 
-    // Print all combination using temporary array 'data[]'
-    combinationUtil(arr, data, 0, n-1, 0, r);
-
-}
- 
-/* arr[]  ---> Input Array
-   data[] ---> Temporary array to store current combination
-   start & end ---> Staring and Ending indexes in arr[]
-   index  ---> Current index in data[]
-   r ---> Size of a combination to be printed */
-void combinationUtil(int arr[], int data[], int start, int end,
-                     int index, int r)
-{
-    FILE *result_combination;
-    result_combination =  fopen("result_combination.txt","a");
-// Current combination is ready to be printed, print it
-    if (index == r)
-    {
-        for (int j=0; j<r; j++)
-        fprintf(result_combination,"%d ", data[j]);
-        fprintf(result_combination,"\n");
-        return;
-    }
-
-    // replace index with all possible elements. The condition
-    // "end-i+1 >= r-index" makes sure that including one element
-    // at index will make a combination with remaining elements
-    // at remaining positions
-    for (int i=start; i<=end && end-i+1 >= r-index; i++)
-    {
-        data[index] = arr[i];
-        combinationUtil(arr, data, i+1, end, index+1, r);
-
-        // Remove duplicates
-        while (arr[i] == arr[i+1])
-             i++;
-    }
-    fclose(result_combination);
-}
- 
-// Driver program to test above functions
-int main(int argc, char **argv, int *arr)
-{
-    FILE *indate;
-
-    /* reading in some parameters and open corresponding files */
-    indate = fopen(argv[1],"r+");
+  FILE *indate, *result_combination;
+  //read date file
+  indate = fopen(argv[1],"r+");
 
     if (indate == NULL) {
         printf("Couldn't open the file.");
@@ -69,25 +17,27 @@ int main(int argc, char **argv, int *arr)
     }
 
     char tmp1[200];
-    int i,n;
-	i=0;
+    int k;
+	k=0;
         while(fscanf(indate, "%s", &tmp1[0])) 
 	{
 		if (feof(indate)) break;
-                arr[i]=atoi(tmp1);
-                i=i+1;
+                arr[k]=atoi(tmp1);
+                k=k+1;
         }
-	printf ("number of i = %i \n", i);
+	printf ("number of dates = %i \n", k); 
 
-    int r = 2;
-//  int n = sizeof(arr)/sizeof(arr[0]);
-    n = i;
-    printf ("size of date array = %i \n", n);
-    
 
-    char filename[] = "result_combination.txt";
-    remove(filename);
-
-    printCombination(arr, n, r);
-
+  //make simple combination and write on file
+  result_combination =  fopen("result_combination.txt","a");
+  //int arr[] = {1, 2, 3};
+  int i,j,*data;
+  data=arr;
+  for (i=0; i<k;i++)
+      for (j=0; j<k; j++) {
+  if (arr[i]!=data[j] && arr[i]<data[j])
+//  printf ("hasil: %d %d \n",arr[i], data[j]);
+  fprintf(result_combination,"%d %d \n",arr[i], data[j]);
+  }
+  return 0;
 }
