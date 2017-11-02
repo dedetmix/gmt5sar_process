75c3e4782c34508957cde760ed74604d9dcf819a
dedetmix <dedetmix@yahoo.co.id>
2017-11-02 16:20:00 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -9,10 +9,11 @@
 ######################################################################
 
 dir=$(pwd)
-#mkdir xml_origin
-#mv *.xml $dir/xml_origin/.
-
-#paste -d\  date_asc_s.txt data_s.in > date_xml.in
+mkdir xml_origin
+mv *.xml $dir/xml_origin/.
+cp ../../date_asc.txt .
+cp ../../data.in .
+paste -d\  date_asc.txt data.in | sed -r 's/[:]+/ /g'> date_xml.in
 
 shopt -s extglob
 IFS=" "
@@ -28,26 +29,27 @@ mv "$xml"_a.xml $xml.xml
 
 done < "$1"
 
-cd ..
-rm -r -f raw
-mkdir raw
-cd raw
-ln -s ../raw_orig/*.PRM
-ln -s ../raw_orig/*.PRM0
-ln -s ../raw_orig/*.SLC
-ln -s ../raw_orig/*.PRM
-ln -s ../raw_orig/*.dat
-ln -s ../raw_orig/*.grd
-cd ../raw_orig
-
-# remember replace symbol ":" to spasi on file date_xml.in
-# atention that aux file could be from sen 1a or sen 1b with IPFV difference
+# make sure to have space " " between column instead of symbol ":" on file date_xml.in
+# attention that aux file could be from sen 1a or sen 1b with IPFV difference
 # input output name has to be different, change the output file name and replace the original
-# rember the callibration file not all of date has it, regarding to IPFV -> read Elevation Antenna Pattern (EAP)
+# remember also the aux callibration is not applied to all interferograms, just the one which has different IPF version, > read Elevation Antenna Pattern (EAP) Contribution
 
 # run prep.sh every aux_cal, remember there are sentinel 1A and sentinel 1B 
 
-# finally run:
+# finally run (on raw_orig folder):
 # --> preproc_batch_tops.csh data.in dem.grd 1 ,this step to prepare SBAS process (baseline_table.dat)
 # --> preproc_batch_tops.csh data.in dem.grd 2
+# then type on terminal
+# $ cd ..
+# $ rm -r -f raw
+# $ mkdir raw
+# $ cd raw
+# $ ln -s ../raw_orig/*.PRM .
+# $ ln -s ../raw_orig/*.PRM0 .
+# $ ln -s ../raw_orig/*.LED .
+# $ ln -s ../raw_orig/*.SLC .
+# $ ln -s ../raw_orig/*.PRM .
+# $ ln -s ../raw_orig/*.dat .
+# $ ln -s ../raw_orig/*.grd .
+# $ cd ..
 # --> go to main directory, run: intf_tops.csh intf.in batch_tops.config
